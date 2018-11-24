5af7a1d28399d320438a0394033fc3bd733ccdc9
Hunter Jefferson <hj@artkade.com>
2018-11-24 14:20:56 -0600

    Made SearchBar controlled component.

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 class Searchbar extends Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render() {
     return (
@@ -11,7 +9,11 @@ class Searchbar extends Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
