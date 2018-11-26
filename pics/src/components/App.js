8fdf1e9dbbd4ba681836b271b1cbb807725a3527
Hunter Jefferson <hj@artkade.com>
2018-11-26 00:08:10 -0600

    Add data to state and print number of search results on screen.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 import Searchbar from './SearchBar';
 
 class App extends Component {
-  async onSearchSubmit(term) {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
@@ -11,13 +13,15 @@ class App extends Component {
           'Client-ID 252c964e842d0c64a49e0efb2a8e9da67f8b48f7d65ea3306ee888fd3df00955'
       }
     });
-    console.log(response.data.results);
-  }
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <Searchbar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
