cb278f4df62022cdfdfe2eda6fe1bccf787eb458
Hunter Jefferson <hj@artkade.com>
2018-11-26 13:07:50 -0600

    Added controlled SearchBar component.

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="searchBar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
