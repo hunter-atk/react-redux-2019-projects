7e2baad5df676513990263d54ad964a553e65b53
Hunter Jefferson <hj@artkade.com>
2018-11-26 01:34:50 -0600

    Added key property to each image list item. Assigned object id to key.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ImageList = props => {
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} />;
+    return <img key={image.id} src={image.urls.regular} />;
   });
   return <div>{images}</div>;
 };
