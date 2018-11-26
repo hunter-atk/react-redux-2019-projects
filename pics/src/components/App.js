c58e2d2e35801d3029b604ba673f4425c78e2245
Hunter Jefferson <hj@artkade.com>
2018-11-25 23:53:28 -0600

    Use async await to console.log data.results object.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 import Searchbar from './SearchBar';
 
 class App extends Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization:
           'Client-ID 252c964e842d0c64a49e0efb2a8e9da67f8b48f7d65ea3306ee888fd3df00955'
       }
     });
+    console.log(response.data.results);
   }
 
   render() {
