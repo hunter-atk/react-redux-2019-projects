c7c240e1a625aece83067622d0e7625bb2cd3cc9
Hunter Jefferson <hj@artkade.com>
2018-11-25 23:42:18 -0600

    Installed axios and now able to send requests to Unsplash API.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import Searchbar from './SearchBar';
 
 class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 252c964e842d0c64a49e0efb2a8e9da67f8b48f7d65ea3306ee888fd3df00955'
+      }
+    });
   }
 
   render() {
