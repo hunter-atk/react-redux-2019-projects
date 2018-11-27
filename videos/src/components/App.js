7d4a18433d302a9e191f80d00d362378c1851feb
Hunter Jefferson <hj@artkade.com>
2018-11-26 20:00:11 -0600

    Got API response and assigned data to state property.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+    console.log(this.state.videos);
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onTermSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component {
     event.preventDefault();
 
     //TO DO: Call callback from parent component.
+    this.props.onTermSubmit(this.state.term);
   };
 
   render() {
